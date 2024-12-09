c7a4ca67f1eec1ea0f0aa597de1ddbecfb8fd482
cjking4 <cjgamefowl032@gmail.com>
2024-12-09 19:31:20 +0100

    almost

--- prove/Develop05/Listing.cs
+++ prove/Develop05/Listing.cs
@@ -1,5 +1,7 @@
+
 using System;
 using System.Net;
+
 public class Listing : Activity
 {
     private int _count;
@@ -11,57 +13,52 @@ public class Listing : Activity
         "Who are people that you have helped this week?",
         "When have you felt the Holy Ghost this month?",
         "Who are some of your personal heroes?",
-        "What makes you want to be a better person everyday"   
+        "What makes you want to be a better person everyday"
     };
-    private List<string> UserResponds;
 
-    public Listing () :base ("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area")
+    
+    private List<string> UserResponds = new List<string>();  
+
+    public Listing() : base("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area")
     {
-       
+        
     }
 
 
-   
     public void Run()
     {
+        DateTime StartTime = DateTime.Now;
+        DateTime EndTime = StartTime.AddSeconds(_Duration);
+
+        while (StartTime < EndTime)
+        {
+            Console.WriteLine("List as many responses as you can to the following prompts");
+            GetRandomPrompt();
+
+            Console.Write("You may begin in ");
+            ShowCountDown(5);
+            Console.WriteLine();
+
+            string response = Console.ReadLine();
+            UserResponds.Add(response);
+
+            StartTime = DateTime.Now; 
+        }
+
+        Console.WriteLine("Time is up! Thank you for participating.");
+
        
-         DateTime StartTime = DateTime.Now;
-         DateTime EndTime = StartTime.AddSeconds(_Duration);
-
-         do
-         {
-          Console.WriteLine("List as many responses as you can to the following prompts");
-         GetRandomPrompt();
-         Console.Write("You may begin in  ");
-         ShowCountDown(5);
-         Console.WriteLine();
-         string response = Console.ReadLine();
-            do{
-                UserResponds.Add(response);
-            }
-            while ( StartTime >= EndTime);
-
-         }
-         while ( StartTime < EndTime);
-         {
-            Thread.Sleep(1000);
-         }
-
     }
+
     public void GetRandomPrompt()
     {
         Random index = new Random();
         int randomPos = index.Next(Listprompts.Count);
         string randomPrompt = Listprompts[randomPos];
-        Console.WriteLine($" --- {randomPrompt} --- "); 
-        Listprompts.Remove(Listprompts[randomPos]);                   
-    }
-    
+        Console.WriteLine($" --- {randomPrompt} --- ");
 
-    
+        
+    }
+
 
 }
-
-
-
-
