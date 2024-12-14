5a1ccc802164a01d2c12e5d9056bf6f45009d72e
cjking4 <cjgamefowl032@gmail.com>
2024-12-14 17:43:28 +0100

    done

--- prove/Develop06/GoalManager.cs
+++ prove/Develop06/GoalManager.cs
@@ -8,7 +8,7 @@ public class GoalManager
 
     public void Start()
     {
-        int choice;
+        string choice;
 
         do
         {
@@ -24,15 +24,40 @@ public class GoalManager
             Console.WriteLine("4.  Load Goals");
             Console.WriteLine("5.  Record Event");
             Console.WriteLine("6.  Quit");
+            Console.Write(" Select a choice from the menu: ");
+            choice = Console.ReadLine();
 
+            if ( choice == "1")
+            {
+                CreateGoal();
+            }
+            else if ( choice == "2")
+            {
+                ListGoalDetails();
+            }
+            else if ( choice == "3")
+            {
+                SaveGoals();
+            }
+            else if ( choice == "4")
+            {
+                Console.Write("What is the name of your file?  ");
+                string fileName = Console.ReadLine();
+                LoadGoals(fileName);
+                Console.WriteLine($"{fileName} has been loded");
+            }
+            else if ( choice == "5")
+            {
+                RecordEvent();
+            }
 
+            else 
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter a valid option !!");
 
-
-
-
-
-
-        }
+            }
+        } while (choice != "6");
 
     }
 
@@ -134,6 +159,89 @@ public class GoalManager
         Console.WriteLine("The goals are: ");
         ListGoalNames();
         Console.Write("Which goal have you completed? ");
+        int get = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        int index = get - 1;
+        int points = _goals[index].RecordEvent();
+
+        if (points == 0)
+        {
+            Console.WriteLine("The goal has already been completed");
+            
+        }
+        else
+        {
+            _Score += points;
+
+        }
+    }
+
+    public void SaveGoals()
+    {
+        Console.Write("With what name will this file be saved?  ");
+        string fileName = Console.ReadLine();
+        Console.WriteLine();
+
+        using StreamWriter outputFile = new(fileName);
+        outputFile.WriteLine(_Score);
+        foreach (Goal goal in _goals)
+        {
+            outputFile.WriteLine(goal.GetStringRepresentation());
+        }
+        Console.WriteLine($"File saved as {fileName}");
+        _Score = 0;
+        Console.WriteLine();
+        _goals.Clear();
+
+
+    }
+
+    public void LoadGoals(string fileName)
+    {
+        string [] lines = System.IO.File.ReadAllLines(fileName);
+
+        if (lines.Length > 0)
+        {
+            int lastScore = int.Parse(lines[0]);
+            _Score = lastScore;
+        }
+
+        foreach ( string line in lines)
+        {
+            string [] parts = line.Split(",");
+            if (parts.Length >= 4)
+            {
+                string type = parts[0];
+                string name = parts[1];
+                string description = parts[2];
+                int points = int.Parse(parts[3]);
+
+                if ( type == "SimpleGoal")
+                {
+                    bool isComplete = bool.Parse(parts[4]);
+                    _goals.Add(new SimpleGoal(name, description, points, isComplete));
+
+                }
+                else if (type == "EternalGoal")
+                {
+                    _goals.Add(new EternalGoal(name, description, points));
+
+                }
+
+                else if (type == "CheckListGoal")
+                {
+                    if (parts.Length >= 6)
+                    {
+                        int target = int.Parse(parts[4]);
+                        int bonus = int.Parse(parts[5]);
+                        int amount = int.Parse(parts[6]);
+                        bool isCompleteCheckList = bool.Parse(parts[7]);
+                        _goals.Add(new CheckListGoal(name, description, points, target, bonus, amount, isCompleteCheckList));
+
+                    }
+                }
+            }
+        }
     }
 
 }
---
+++ prove/Develop06/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GoalManager goalManager = new();
+        goalManager.Start();
+    }
+}
